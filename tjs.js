776edbc8e4b6c4a9451c3aad547b12c32882b66d
wipedlifepotato <60944239+wipedlifepotato@users.noreply.github.com>
2026-08-24 11:23:13 +0300

    tjs library example

--- tjs.js
+++ tjs.js
@@ -1,39 +1,67 @@
-const TorrentWrapper = Object()
-TorrentWrapper.client_link = (host="127.0.0.1", port=9191, t="mytorrents") => {
-    return `http://${host}:${port}/${t}/rpc/`;
-}
-TorrentWrapper.add = async () => {
-    const res = await fetch(TorrentWrapper.client_link(), {
-        method: "POST",
-        body: JSON.stringify({ 'method': 'torrent-add', 'arguments': { 'metainfo': b64metainfo } }),
-                            headers: { 'Content-Type': 'application/json' }
-    });
-    return res.json()
+class TorrentClient {
+    constructor(host = '127.0.0.1', port = 9191, path = 'mytorrents') {
+        this.host = host;
+        this.port = port;
+        this.path = path;
+    }
+
+    setConnection(host, port, path) {
+        this.host = host;
+        this.port = port;
+        this.path = path;
+    }
+
+    getEndpoint() {
+        return `http://${this.host}:${this.port}/${this.path}/rpc/`;
+    }
+
+    getStatusString(status) {
+        switch (status) {
+            case 4: return 'DOWNLOADING';
+            case 6: return 'SEEDING';
+            case 0: return 'STOPPED';
+            case 2: return 'CHECKING';
+            default: return 'QUEUED';
+        }
+    }
+
+    formatBytes(bytesPerSec) {
+        if (!bytesPerSec) return '0 B/s';
+        if (bytesPerSec > 1024 * 1024) {
+            return (bytesPerSec / (1024 * 1024)).toFixed(2) + ' MB/s';
+        }
+        return (bytesPerSec / 1024).toFixed(2) + ' KB/s';
+    }
+
+    async addTorrent(b64metainfo) {
+        const res = await fetch(this.getEndpoint(), {
+            method: "POST",
+            body: JSON.stringify({ 'method': 'torrent-add', 'arguments': { 'metainfo': b64metainfo } }),
+                                headers: { 'Content-Type': 'application/json' }
+        });
+        return res.json();
+    }
+
+    async removeTorrent(id) {
+        const res = await fetch(this.getEndpoint(), {
+            method: "POST",
+            body: JSON.stringify({ 'method': 'torrent-remove', 'arguments': { 'ids': [id], 'delete-local-data': true } }),
+                                headers: { 'Content-Type': 'application/json' }
+        });
+        return res.json();
+    }
+
+    async getTorrents() {
+        const res = await fetch(this.getEndpoint(), {
+            method: "POST",
+            body: JSON.stringify({
+                'method': 'torrent-get',
+                'arguments': { "fields": ["id", "name", "status", "rateDownload", "rateUpload", "totalSize", "percentDone"] }
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        return res.json();
+    }
 }
 
-TorrentWrapper.remove = async () => {
-    const res= await fetch(TorrentWrapper.client_link(), {
-        method: "POST",
-        body: JSON.stringify({ 'method': 'torrent-remove', 'arguments': { 'ids': [id], 'delete-local-data': true } }),
-                           headers: { 'Content-Type': 'application/json' }
-    });
-    return res.json()
-}
-
-TorrentWrapper.get = async (fields = ["id", "name", "status", "rateDownload", "rateUpload", "totalSize", "percentDone"]) => {
-    const res = await fetch(TorrentWrapper.client_link(), {
-        method: "POST",
-        body: JSON.stringify({ 'method': 'torrent-get', 'arguments': { "fields": fields } }),
-                            headers: { 'Content-Type': 'application/json' }
-    });
-    return res.json();
-}
-
-TorrentWrapper.exampleProccessFile = () => {
-    const reader = new FileReader();
-    reader.onload = async () => {
-        const b64 = btoa(String.fromCharCode(...new Uint8Array(reader.result)));
-        await torrent_add(b64);
-    };
-    reader.readAsArrayBuffer(file);
-}
+window.tjs = new TorrentClient();
